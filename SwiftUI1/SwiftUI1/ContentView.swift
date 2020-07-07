122fbbd0be6d8a1fc5567b13679ea3a4276c6669
mengxiangjian <mengxiangjian@mengxiangjiandeMacBook-Pro.local>
2020-07-07 18:41:59 +0800

    first commit

---
+++ SwiftUI1/SwiftUI1/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI1
+//
+//  Created by mengxiangjian on 2020/7/7.
+//  Copyright © 2020 mengxiangjian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Turtle Rock")
+            .font(.title)
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
